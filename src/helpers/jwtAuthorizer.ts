8a7c703294ddfb660080aed8d405f4eefd8fa5fd
Hossam Abubakr <sam.baker990@gmail.com>
2022-01-17 23:33:39 +0200

    Updated jwtAuthorizer to tolerate type coercion - Fixed a bug in the payload name

--- src/helpers/jwtAuthorizer.ts
+++ src/helpers/jwtAuthorizer.ts
@@ -5,7 +5,7 @@ export default function Authorize(req: Request, user_id: string | null = null) {
   const authorizationHeader = req.headers.authorization;
   const token = authorizationHeader!.split(" ")[1];
   const decoded = verify(token as string, process.env.TOKEN_SECRET as string) as JwtPayload;
-  if (user_id && decoded.user.id !== user_id) {
+  if (user_id && decoded.user.userId != user_id) {
     throw new Error("User id does not match!");
   }
 }
